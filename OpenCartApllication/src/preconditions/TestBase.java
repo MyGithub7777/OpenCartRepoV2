f1de9f10fc08f51ed98d5355ed6bd6e94c22b55c
MyGithub7777 <asmygithub@gmail.com>
2023-10-23 08:16:29 +0530

    Created Project Structure

---
+++ OpenCartApllication/src/preconditions/TestBase.java
@@ -0,0 +1,13 @@
+package preconditions;
+
+public class TestBase {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Setup method");
+		
+		System.out.println("Tear methiod");
+		
+		System.out.println("Aded dependecnies in pom.xml");
+	}
+}
